4a3e1b04bcfbad349d6c3b90d396a782bbdfc21a
Veronica Escobar <vescobars2001@gmail.com>
2022-04-05 11:30:27 -0500

    Rellene lo de opcion 2 para que creara el buffeer, las threads y tambien las iniciara, faltaria que despues de que las threads terminen ahi le saque la info de fallos de paginas y demas del buffer. Tambien cree las bases de thread 1 y basicamente lo unico que seria es crear en el buffer eel metodo que de hecho actualiza los accesos a pagina. Ya me voy a poner a crear el contendio base de thread 2 y despues del buffer

--- Proyecto-Caso-2/src/Proyect/Thread1.java
+++ Proyecto-Caso-2/src/Proyect/Thread1.java
@@ -1,16 +1,56 @@
 package Proyect;
 
-public class Thread1 {
+import javax.swing.text.Element;
+import java.util.ArrayList;
+
+/**
+ * This is responsible for iterating through the reference table and inserting the latest page reference in the frame page table.
+ * It handles all posible scenarios, such as inserting a page in an empty slot in the frame page table, a fail in page insertion (and the swap)
+ * This should run until the whole of the page reference table has been finished
+ */
+public class Thread1 extends Thread{
     //--------------------------------------------------------------------------
     // Attributes
     //--------------------------------------------------------------------------
+
+    /*
+    The buffer that contains the frame page table
+     */
+    public Buffer buffer;
+
+    /*
+    Table of references
+     */
+    public ArrayList<ElementInfo> referenceTable;
 
     //--------------------------------------------------------------------------
     // Constructor
     //--------------------------------------------------------------------------
 
+    /**
+     * The constructor of thread 1.
+     * @param referenceTable the table of references produced by the execution of the run type
+     */
+    public Thread1(ArrayList<ElementInfo> referenceTable, Buffer buffer){
+        this.referenceTable = referenceTable;
+        this.buffer = buffer;
+    }
+
     //--------------------------------------------------------------------------
     // Methods
     //--------------------------------------------------------------------------
+    /**
+     * The run method that the thread will execute
+     */
+    @Override
+    public void run(){
+        //TODO: Contenido del run. Se muere cuando se le terminaron las paginas que revisar
+        for(int i = 0 ; i < referenceTable.size(); i++){
+            ElementInfo currentElement = referenceTable.get(i);//Gets the element
+            int currentPageReference = currentElement.getPageNumber();//gets the page where the current element referenced is located
 
+            //TODO: Llamar al metodo sincronziado en el buffer que intenta acceder la tabla de paginas para actualizar lso valroes. Seguramente el metodo que directamente accede no esta sincronizado pero llama otros elementos dentro de buffer que monitorean el acceso a la tabla. Ver como funciona eso
+            //buffer.addPageMethod(currentPageReference);
+        }
+    }
 }
--- Proyecto-Caso-2/src/Proyect/Thread2.java
+++ Proyecto-Caso-2/src/Proyect/Thread2.java
@@ -1,16 +1,35 @@
 package Proyect;
 
-public class Thread2 {
+public class Thread2 extends Thread {
     //--------------------------------------------------------------------------
     // Attributes
     //--------------------------------------------------------------------------
+
+    /*
+    The buffer that contains the frame page table
+     */
+    public Buffer buffer;
 
     //--------------------------------------------------------------------------
     // Constructor
     //--------------------------------------------------------------------------
 
+    /**
+     * Constructor of thread 2. This is the thread that is responsible for the execution of the aging algorithm on the frame page table
+     */
+    public Thread2() {
+
+    }
+
     //--------------------------------------------------------------------------
     // Methods
     //--------------------------------------------------------------------------
 
+    /**
+     * The run method that the thread will execute
+     */
+    @Override
+    public void run(){
+        //TODO: Contenido del run. Correr hasta que termine de iterar a traves de toda la lista.
+    }
 }
