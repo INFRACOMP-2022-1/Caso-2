a9bf60af34fd9f37d64cc1edbe56b8c3e27c1ca6
Veronica Escobar <vescobars2001@gmail.com>
2022-04-05 00:04:46 -0500

    Restaure option2 a su estado base

--- Proyecto-Caso-2/src/Proyect/Option2.java
+++ Proyecto-Caso-2/src/Proyect/Option2.java
@@ -21,75 +21,11 @@ public class Option2 {
     public Option2(Configuration configuration, int pageFrameSize) {
         this.configuration = configuration;
         this.pageFrameSize = pageFrameSize;
-        creacionPaginas(pageFrameSize);
-
-
-
     }
 
     //--------------------------------------------------------------------------
     // Methods
     //--------------------------------------------------------------------------
-
-
-
-    public synchronized void  addPage(String message) throws  InterruptedException{
-
-        while( comprobacion()){
-            try {
-                wait();// Makes the incoming message wait (passive) until someone wakes it up
-            }
-            catch (InterruptedException e){
-                e.printStackTrace();
-            }
-        }
-
-
-        addpage(message); //Stores the received message in the buffer content linked list
-
-        synchronized (this){
-            notify();
-        }
-        //notify(); //notifies a random producerConsumer in monitor queue to wake up
-    }
-
-
-
-    public boolean  comprobacion() {
-        boolean rta = false;
-        
-         for(int j= 0; j< TPAGES.size(); j++){
-    
-             if (TPAGES.get(j).get(0) == 0) {
-                rta= true;
-            
-             }
-         }
-         return rta;      
-    }
-
-
-  
-
-   
-
-
-
-    public void  creacionPaginas(int num)
-	{	
-        for(int i= 0; i < num; i++){
-
-	        	Integer l1= 0;
-	        	Integer l2= 0;      	
-	        	ArrayList<Integer> l3= new ArrayList<>();	
-	        	l3.add(l1);
-	        	l3.add(l2);   	
-	        	TPAGES.add(l3);
-	        }
-System.out.println(TPAGES);
-	
-
-	}
 
 
 }
