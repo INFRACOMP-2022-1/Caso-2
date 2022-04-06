be21f06c600e48354a79945ffa86991afcd262ce
Veronica Escobar <vescobars2001@gmail.com>
2022-04-05 23:11:42 -0500

    Hice que opcion 2 escribiera el reporte en un txt

--- Proyecto-Caso-2/src/Proyect/Option2.java
+++ Proyecto-Caso-2/src/Proyect/Option2.java
@@ -1,6 +1,10 @@
 package Proyect;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Option2 {
 
@@ -33,12 +37,11 @@ public class Option2 {
         thread1.start();//TODO: Terminar de crear la clase thread1
         thread2.start();//TODO: Terminar de crear la clase thread2
 
-
         //Waits for both thread 1 and 2 to finish execution
         thread1.join();
         thread2.join();
 
-        //TODO: Output reprote de resultados (fallos de pagina etc.) . Esta info tiene que ser extraida del buffer
+        //Output report
         option2ResultConsole();
     }
 
@@ -65,6 +68,52 @@ public class Option2 {
 
         System.out.println("For more information on the configuration file you can check in option1OutputFiles/" + configuration.configurationName + ".txt" );
 
+    }
+
+    public void createReportFile(){
+        try {
+            Random rand = new Random(); //instance of random class
+            int int_random = rand.nextInt(1000);
+
+            String fileRoute = String.format("Proyecto-Caso-2/option2OutputFiles/%s%s.txt", configuration.configurationName,int_random );
+            File resultFile = new File(fileRoute);
+            if (resultFile.createNewFile()) {
+                System.out.println("You can see the results in the following file in the option2OutputFiles folder: " + resultFile.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            //GET FILE
+            FileWriter fileWriter = new FileWriter(fileRoute);
+            fileWriter.write("\n");
+            fileWriter.write("REPORT");
+            fileWriter.write("\n");
+
+            //WRITE REPORT CONTENT
+
+            //configuration
+            fileWriter.write("CONFIGURATION:\n");
+            fileWriter.write(String.format("TP=%d\n",configuration.pageSize));
+            fileWriter.write(String.format("TE=%d\n",configuration.intSize));
+            fileWriter.write(String.format("NF=%d\n",configuration.rows));
+            fileWriter.write(String.format("NC=%d\n",configuration.columns));
+            fileWriter.write(String.format("TR=%d\n",configuration.runType));
+            fileWriter.write(String.format("NP=%d\n",configuration.pageNumber));
+            fileWriter.write(String.format("NR=%d\n\n",configuration.totalReferences));
+
+            //option 2 info
+
+            fileWriter.write("MARCOS DE PAGINA:\n");
+            fileWriter.write("Page frames(Marcos de pagina): " + buffer.pageFrames + "\n");
+
+            fileWriter.write("FALLOS DE PAGINA:\n");
+            fileWriter.write("Page errors(Fallos de pagina): " + buffer.pageErrors + "\n");
+
+            fileWriter.write("For more information on the configuration file you can check in option1OutputFiles/" + configuration.configurationName + ".txt" );
+
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
